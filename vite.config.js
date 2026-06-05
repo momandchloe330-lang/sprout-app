f7b4d7e97b40161030bdac770de4c85c15f7dd28
momandchloe330-lang <momandchloe330@gmail.com>
2026-06-05 12:44:28 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['ios >= 12', 'safari >= 12', 'chrome >= 64'],
-      modernPolyfills: true,
-    }),
-  ],
+    plugins: [react()],
 })
